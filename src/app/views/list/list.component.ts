8428e4a6ff0014df4140eb2d93ee620d61b2f17d
Welington Cruz <welingtoncassis@gmail.com>
2019-07-02 16:07:13 -0300

    primeiro upload

---
+++ src/app/views/list/list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  selectedPkm = null;
+  pokemonList = [
+    { name: 'Bulbasaur', number: 1 },
+    { name: 'Charmander', number: 4 },
+    { name: 'Squirtle', number: 7 },
+    { name: 'Pikachu', number: 25 }
+  ];
+
+  get pkmSprite() {
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  selectPokemon(pkm) {
+    this.selectedPkm = pkm;
+  }
+
+}
