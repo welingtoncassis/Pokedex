173fd1777b95519b9243ff30d900d7a27f8923e0
Welington Cruz <welingtoncassis@gmail.com>
2019-07-02 16:56:35 -0300

    criando serviço para consumir a api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { ListComponent } from './views/list/list.component';
     ListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-list',
@@ -7,20 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  nameFilter = '';
   selectedPkm = null;
-  pokemonList = [
-    { name: 'Bulbasaur', number: 1 },
-    { name: 'Charmander', number: 4 },
-    { name: 'Squirtle', number: 7 },
-    { name: 'Pikachu', number: 25 }
-  ];
+
+  constructor(
+    private pokeapi: PokeapiService
+  ) { }
+
+
+  get pokemonList() {
+    return this.pokeapi.pokeList.filter( pokemon => {
+      return pokemon.name.toLocaleLowerCase().indexOf(this.nameFilter.toLocaleLowerCase()) !== -1;
+    });
+  }
 
   get pkmSprite() {
+  // tslint:disable-next-line: variable-name
     const number = ('000' + this.selectedPkm.number).slice(-3);
     return `//serebii.net/sunmoon/pokemon/${number}.png`;
   }
-
-  constructor() { }
 
   ngOnInit() {
   }
