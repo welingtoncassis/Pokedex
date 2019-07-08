867f4c38c443878c67bdfa5ebbccf1a957dd2018
Welington Cruz <welingtoncassis@gmail.com>
2019-07-08 18:32:26 -0300

    Componentizando a lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ListComponent } from './views/list/list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ListItemComponent } from './componetes/list-item/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
